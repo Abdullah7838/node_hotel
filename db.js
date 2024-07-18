1a098d22134decdb0f6e9d219f55f7fa6bc92100
M.Abdullah <abdullahluqmanbro9@gmail.com>
2024-07-18 14:45:16 +0500

    Server mongoDB added

--- db.js
+++ db.js
@@ -1,26 +1,26 @@
 const mongoose = require("mongoose");
 require('dotenv').config();
-
+//const mongoURL=process.env.DBURL;
+//const mongoURL = "mongodb://localhost:27017/hotel";
 const mongoURL = process.env.DBURL;
 
-mongoose.connect(mongoURL)
-  .then(() => {
-    console.log('Connected to DB');
-  })
-  .catch((error) => {
-    console.error('Error connecting to DB', error);
-  });
+mongoose.connect(mongoURL,{
+    useNewUrlParser :true,
+    useUnifiedTopology:true,
+    
+})
 
 const db = mongoose.connection;
 
-db.on('disconnected', () => {
-  console.log('Disconnected from DB');
-});
-
-db.on('error', (error) => {
-  console.log('Internal Server Error', error);
-});
-
-module.exports = {
-  db
-};
+db.on('connected',()=>{
+    console.log('Connected to DB');
+})
+db.on('disconnected',()=>{
+    console.log('Disconnected to DB');
+})
+db.on('error',()=>{
+    console.log('Internal Server Error');
+})
+module.exports={
+    db
+}
